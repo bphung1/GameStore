839671c3bf28505beed11b452de057b1ad99f680
Bao Phung <b.giaphung@gmail.com>
2020-09-21 15:45:15 -0500

    implementing body to classes

---
+++ GameStoreApp/GameStore.Library/Model/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameStore.Library.Interface
+{
+    class Customer
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public int CustomerID { get; set; }
+        public string FirstName {
+            get => _firstName;
+            set
+            {
+                if (value.Length == 0)
+                {
+                    throw new ArgumentException("First name must not be empty.", nameof(value));
+                }
+                _firstName = value;
+            }
+        }
+    }
+}
